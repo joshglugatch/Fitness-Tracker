f7341d6b08c465b0f8670cb6cc579fd1cc1abd62
Josh Glugatch <joshglugatch@gmail.com>
2020-11-06 17:22:10 -0800

    update model structure

--- models/workout.js
+++ models/workout.js
@@ -2,45 +2,18 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema(
-  {
-    day: {
+const workoutSchema = new Schema({
+  
+    createdAt: {
       type: Date,
       default: Date.now
     },
-    exercises: [
-      {
-        type: {
-          type: String,
-          trim: true,
-          required: "Choose an exercise type"
-        },
-        name: {
-          type: String,
-          trim: true,
-          required: "Choose an exercise name"
-        },
-        duration: {
-          type: Number,
-          required: "Choose an exercise duration in minutes"
-        },
-        weight: {
-          type: Number
-        },
-        reps: {
-          type: Number
-        },
-        sets: {
-          type: Number
-        },
-        distance: {
-          type: Number
-        }
-      }
-    ]
-  },
+    exercises:{
+      type: Array,
+      default: []
+    } 
   
-);
+});
 
 
 
