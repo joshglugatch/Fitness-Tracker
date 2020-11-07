09ddd589fe6f252725aa994f3c7efcf613b2bf6a
Josh Glugatch <joshglugatch@gmail.com>
2020-11-06 18:08:06 -0800

    create virtual for total duration

--- models/workout.js
+++ models/workout.js
@@ -2,20 +2,38 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema({
-  
-    createdAt: {
+const workoutSchema = new Schema(
+  {
+    day: {
       type: Date,
-      default: Date.now
+      default: Date.now()
     },
     exercises:{
       type: Array,
       default: []
-    } 
-  
+    }
+  },
+  {
+    toJSON: {
+      virtuals: true
+    }
+  } 
+);
+
+workoutSchema.virtual("totalDuration").get(function() {
+  return this.exercises.reduce(function (total, exercise) {
+      return total + exercise.duration;
+    }, 0);
 });
 
-
+// workoutSchema.methods.addTotal = function() {
+//   total = 0;
+//   this.exercises.forEach(exercise => {
+//     total += exercise[this.exercises.length-1].duration
+//   }); 
+//   this.totalDuration = total
+//   return this.totalDuration;
+// };
 
 const Workout = mongoose.model("Workout", workoutSchema);
 
